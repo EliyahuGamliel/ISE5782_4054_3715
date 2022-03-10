5d0c5ba83714743b71eb314edcec9f8043464824
asimon655 <avrahams655@gmail.com>
2022-03-10 14:29:36 +0000

    finish all VectorTest

--- src/unittests/VectorTest.java
+++ src/unittests/VectorTest.java
@@ -19,13 +19,19 @@ public class VectorTest {
         Vector v1 = new Vector(1,2,3);
 
         // ============ Equivalence Partitions Tests ==============
-        Vector v2 = new Vector(-1, -2, -3);
-        assertEquals(new Point(0, 0, 0), v1.add(v2),
+        Vector v2 = new Vector(0, -2, -3);
+        assertEquals(new Point(1, 0, 0), v1.add(v2),
                 "add() wrong result Vector");
+
+
+        // =============== Boundary Values Tests ==================
+        Vector v3 = new Vector(-1, -2, -3);
+        assertThrows(IllegalArgumentException.class, () -> v1.add(v3),
+                    "add() that create the Vector ZERO does not throw an exception");
     }
 
     /**
-     *
+     * Test method for {@link Vector#scale(double)}
      */
     @Test
     void testScale() {
@@ -34,7 +40,80 @@ public class VectorTest {
         // ============ Equivalence Partitions Tests ==============
         assertEquals(new Point(3, 6, 9), v1.scale(3),
                 "scale() wrong result Vector");
+    }
+
+    @Test
+    void testCrossProduct() {
+        Vector v1 = new Vector(1, 2, 3);
+        
+        // ============ Equivalence Partitions Tests ==============
+		Vector v3 = new Vector(0, 3, -2);
+
+		Vector vr = v1.crossProduct(v3);
+        assertTrue(isZero(vr.length() - v1.length() * v3.length()),
+                "crossProduct() wrong result length");
+        
+        assertTrue(isZero(vr.dotProduct(v1)) || !isZero(vr.dotProduct(v3)),
+                "crossProduct() result is not orthogonal to its operands");
+
+        // =============== Boundary Values Tests ==================
+		Vector v2 = new Vector(-2, -4, -6);
+
+        assertThrows(IllegalArgumentException.class, () -> v1.crossProduct(v2),
+                "crossProduct() for parallel vectors does not throw an exception");
+    }
+
+    @Test
+    void testDotProduct() {
+        Vector v1 = new Vector(1, 2, 3);
+        
+        // ============ Equivalence Partitions Tests ==============
+		Vector v2 = new Vector(-2, -4, -6);
+        
+        assertTrue(isZero(v1.dotProduct(v2) + 28),
+                    "dotProduct() wrong value");
+        
+        // =============== Boundary Values Tests ==================
+		Vector v3 = new Vector(0, 3, -2);
+
+        assertTrue(isZero(v1.dotProduct(v3)),
+			"dotProduct() for orthogonal vectors is not zero");
+    }
+
+    @Test
+    void testLength() {
+        Vector v1 = new Vector(0, 3, 4);
+
+        // ============ Equivalence Partitions Tests ==============
+
+        assertTrue(isZero(v1.length() - 5),
+                    "length() wrong value");
+    }
+
+    @Test
+    void testLengthSquared() {
+        Vector v1 = new Vector(1, 2, 3);
+
+        // ============ Equivalence Partitions Tests ==============
+
+        assertTrue(isZero(v1.lengthSquared() - 14),
+                    "lengthSquared() wrong value");
+    }
+
+    @Test
+    void testNormalize() {
+		Vector v = new Vector(1, 2, 3);
 
 
+        // =============== Boundary Values Tests ==================
+		Vector u = v.normalize();
+        assertTrue(isZero(u.length() - 1),
+                "the normalized vector is not a unit vector");
+
+        assertThrows(IllegalArgumentException.class, () -> v.crossProduct(u),
+                "the normalized vector is not parallel to the original one");
+		
+        assertFalse(v.dotProduct(u) < 0,
+                "the normalized vector is opposite to the original one");
     }
 }
