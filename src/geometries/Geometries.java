ae51f919921e8032dd0c6e796ffa38709516ec30
asimon <avrahams655@gmail.com>
2022-03-20 04:37:40 +0200

    geometries findIntersections

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -4,6 +4,8 @@ import primitives.Point;
 import primitives.Ray;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Geometries implements Intersectable {
@@ -14,18 +16,26 @@ public class Geometries implements Intersectable {
     }
 
     public Geometries(Intersectable... geometries) {
-        this();
+        this.geometries = new ArrayList<>(Arrays.asList(geometries));
     }
 
     public void add(Intersectable... geometries) {
-        for (var geometry:geometries) {
-            this.geometries.add(geometry);
-        }
+        this.geometries.addAll(Arrays.asList(geometries));
     }
 
     @Override
     public List<Point> findIntersections(Ray ray) {
-        return null;
+        List<Point> res = null;
+        for (Intersectable geometry : this.geometries) {
+            List<Point> resi = geometry.findIntersections(ray);
+            if (resi != null) {
+                if (res == null) {
+                    res = new LinkedList<Point>();
+                }
+                res.addAll(resi);
+            }
+        }
+        return res;
     }
 
 }
--- unittests/geometries/GeometriesTest.java
+++ unittests/geometries/GeometriesTest.java
@@ -13,14 +13,15 @@ class GeometriesTest {
 
     @Test
     void findIntersections() {
+        Geometries geometries = new Geometries(new Sphere(new Point(1,1,1), 1),
+                new Plane(new Point(-4, 4, 1), new Vector(-2, -2, -1)),
+                new Triangle(new Point(-0.5, -1.2, 0), new Point(-1, 1, 0.5), new Point(-1, 0.5, -1)));
 
         // ============ Equivalence Partitions Tests ==============
         //TC01: Some shapes are cut (but not all)
-        Geometries geometries = new Geometries(new Sphere(new Point(1,1,1), 1),
-                new Plane(new Point(-4, 4, 1), new Vector(-2, -2, -1)));
         Ray r1 = new Ray(new Point(0,0,0), new Vector(1.5,1,1));
-        List<Point> result = geometries.findIntersections(r1);
-        assertEquals(result.size(), 2, "Wrong number of points");
+        List<Point> res1 = geometries.findIntersections(r1);
+        assertEquals(3, res1.size(), "Wrong number of points");
 
         // =============== Boundary Values Tests ==================
         //TC11: There are no shapes
@@ -29,12 +30,19 @@ class GeometriesTest {
         assertNull(g4.findIntersections(r4), "There are no shapes");
 
         //TC12: All shapes are cut
-        Ray r5 = new Ray(new Point(-1, -1, 1), new Vector(0, -1, 3));
+        Ray r5 = new Ray(new Point(-2, 0, -0.5), new Vector(6, 3, 2.5));
+        List<Point> res2 = geometries.findIntersections(r5);
+        assertEquals(4, res2.size(), "All shapes are cut");
 
         //TC13: All shapes are not cut
-        Ray r6 = new Ray(new Point(-1, -1, 1), new Vector(3, 5, 1.5));
+        Ray r6 = new Ray(new Point(-1, -1, 1), new Vector(-3, -5, -1.5));
+        List<Point> res3 = geometries.findIntersections(r6);
+        assertNull(res3, "All shapes are not cut");
 
         //TC14: Only one shape is cut
+        Ray r7 = new Ray(new Point(-1, -1, 1), new Vector(1, 1, 1));
+        List<Point> res4 = geometries.findIntersections(r7);
+        assertEquals(1, res4.size(), "Only one shape is cut");
 
     }
 }
