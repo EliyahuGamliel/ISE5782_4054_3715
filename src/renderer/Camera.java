61916ee9a4e49db40a0c0bec3d45b2345d7ef15c
asimon <avrahams655@gmail.com>
2022-03-22 15:43:13 +0200

    java doc for Cemera

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -3,12 +3,22 @@ package renderer;
 import primitives.*;
 import static primitives.Util.*;
 
+/**
+ * Class to represent a Camera in 3d space
+ * camera has a View Plane and can make rays throu that View Plane
+ */
 public class Camera {
     private Point location;
     private Vector vTo, vUp, vRight;
     private double width, height;
     private double distance;
 
+    /**
+     * create a camera specifying the location and the To and Up vectors
+     * @param location the camera location
+     * @param vTo the towords direction
+     * @param vUp the up direction
+     */
     public Camera(Point location, Vector vTo, Vector vUp) {
         if (! isZero(vTo.dotProduct(vUp))) {
             throw new IllegalArgumentException("vUp and vTo are not orthogonal");
@@ -41,17 +51,34 @@ public class Camera {
     public double getDistance() {
         return distance;
     }
-    
+    /**
+     * set the View Plane size
+     * @param width the width of the VP
+     * @param height the height of the VP
+     * @return self
+     */
     public Camera setVPSize(double width, double height) {
         this.width = width;
         this.height = height;
         return this;
     }
+    /**
+     * set the View Plane distance from the camera
+     * @param distance the VP distance from the camera
+     * @return self
+     */
     public Camera setVPDistance(double distance) {
         this.distance = distance;
         return this;
     }
-    
+    /**
+     * create a ray from the camera throu a specific pixel in the View Plane
+     * @param nX how many pixels are in the X dim
+     * @param nY how many pixels are in the Y dim
+     * @param j the pixel to go throu X dim 
+     * @param i the pixel to go throu Y dim 
+     * @return the constructed Ray
+     */
     public Ray constructRay(int nX, int nY, int j, int i) {
         return null;
     }
