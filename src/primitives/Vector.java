ed7b8a49ab4712efcaedcf76e2cf7cd637d7a697
asimon <avrahams655@gmail.com>
2022-05-09 11:43:47 +0300

    add rotate method

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -106,4 +106,17 @@ public class Vector extends Point {
                 xyz.d3*vector.xyz.d1-xyz.d1*vector.xyz.d3,
                 xyz.d1*vector.xyz.d2-xyz.d2*vector.xyz.d1);
     }
+
+    public   Vector   Roatate ( double   angle  ,  Vector   axis  ){ 
+        angle  =  angle  /  180  *  Math . PI  ;  
+        double   cosa  =  Math . cos ( angle  ) ,  sina  =  Math . sin ( angle );  
+        double    x  =  axis . xyz . d1  ,  y  =  axis . xyz . d2  ,  z = axis . xyz . d3  , x2  =  x * x  , y2  = y * y ,  z2  =  z * z ;  
+        double   tx  =  this . xyz . d1  ,  ty  =  this . xyz . d2  , tz  = this . xyz . d3  ;  
+        return   new   Vector ( 
+          ( x2 *( 1 - cosa )+  cosa  )* tx  +  ( x * y *( 1 - cosa )- sina )* ty  + ( x * z *( 1 - cosa )+ y * sina )* tz  ,  
+          ( x * y *( 1 - cosa )+ z * sina )* tx  + ( y2 *( 1 - cosa )+ cosa )* ty  + ( y * z *( 1 - cosa )- x * sina )* tz  ,  
+          ( x * z *( 1 - cosa )- y * sina )* tx  + ( y * z *( 1 - cosa )+ x * sina )* ty  + ( z2 *( 1 - cosa )+ cosa )* tz   
+       ); 
+
+   }
 }
