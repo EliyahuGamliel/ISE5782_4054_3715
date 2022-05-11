0ac653e09125440f99ea4145076d68980b07a042
asimon <avrahams655@gmail.com>
2022-05-11 17:07:05 +0300

    add lights on the launch pad

--- unittests/renderer/RenderTests.java
+++ unittests/renderer/RenderTests.java
@@ -16,6 +16,8 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -259,11 +261,22 @@ public class RenderTests {
 
 	@Test
 	void coolImageTest() {
+		Vector axisY = new Vector(0, 1, 0);
+
 		Geometries constGeometries = new Geometries();
+		List<LightSource> constLights = new ArrayList<>();
+
+		Geometries redLightsSurfaces = new Geometries();
+		Geometries greenLightsSurfaces = new Geometries();
+		Geometries blueLightsSurfaces = new Geometries();
+		
+		List<LightSource> redLights = new ArrayList<>();
+		List<LightSource> greenLights = new ArrayList<>();
+		List<LightSource> blueLights = new ArrayList<>();
 
 		hellicopter choper = new hellicopter(new Point(0, 0, 0), 8);
 		//add a plane to geometries as a background
-		constGeometries.add(new Plane(new Point(0, -100, -200), new Vector(0, 1, 0))
+		constGeometries.add(new Plane(new Point(0, -100, -200), axisY)
 									.setEmission(new Color(155, 118, 83))
 									.setMaterial(new Material().setkD(0.2).setkS(0.5).setShininess(100)));
 
@@ -272,50 +285,80 @@ public class RenderTests {
 									.setMaterial(new Material().setkD(0.2).setkS(0.5).setShininess(100)));
 
 		//draw an H at the floor background
-		constGeometries.add(createRectangleY(new Point(0, -100+0.9, 25), 80, 18)
+		constGeometries.add(createRectangleY(new Point(0, -100+0.09, 25), 80, 18)
 									.setEmission(new Color(58, 58, 58))
 									.setMaterial(new Material().setkD(0.5).setkS(0.5).setShininess(300)));
-		constGeometries.add(createRectangleY(new Point(0, -100+0.9, -25), 80, 18)
+		constGeometries.add(createRectangleY(new Point(0, -100+0.09, -25), 80, 18)
 									.setEmission(new Color(58, 58, 58))
 									.setMaterial(new Material().setkD(0.5).setkS(0.5).setShininess(300)));
-		constGeometries.add(createRectangleY(new Point(0, -100+0.9, 0), 20, 50)
+		constGeometries.add(createRectangleY(new Point(0, -100+0.09, 0), 20, 50)
 									.setEmission(new Color(58, 58, 58))
 									.setMaterial(new Material().setkD(0.5).setkS(0.5).setShininess(300)));
 
-		constGeometries.add(createRectangleY(new Point(0, -100+1, 25), 74, 12)
+		constGeometries.add(createRectangleY(new Point(0, -100+0.1, 25), 74, 12)
 									.setEmission(new Color(150, 150, 150))
 									.setMaterial(new Material().setkD(0.5).setkS(0.5).setShininess(300)));
-		constGeometries.add(createRectangleY(new Point(0, -100+1, -25), 74, 12)
+		constGeometries.add(createRectangleY(new Point(0, -100+0.1, -25), 74, 12)
 									.setEmission(new Color(150, 150, 150))
 									.setMaterial(new Material().setkD(0.5).setkS(0.5).setShininess(300)));
-		constGeometries.add(createRectangleY(new Point(0, -100+1, 0), 14, 50)
+		constGeometries.add(createRectangleY(new Point(0, -100+0.1, 0), 14, 50)
 									.setEmission(new Color(150, 150, 150))
 									.setMaterial(new Material().setkD(0.5).setkS(0.5).setShininess(300)));
 
 		//draw an circel around the H at the floor background				
-		constGeometries.add(new Circle(new Point(0, -100+0.5, 0), new Vector(0, 1, 0), 81)
+		constGeometries.add(new Circle(new Point(0, -100+0.05, 0), axisY, 81)
 									.setEmission(new Color(58, 58, 58))
 									.setMaterial(new Material().setkD(0.5).setkS(0.5).setShininess(300)));
 
-		constGeometries.add(new Circle(new Point(0, -100+0.6, 0), new Vector(0, 1, 0), 78)
+		constGeometries.add(new Circle(new Point(0, -100+0.06, 0), axisY, 78)
 									.setEmission(new Color(255, 211, 25))
 									.setMaterial(new Material().setkD(0.5).setkS(0.5).setShininess(300)));
 
-		constGeometries.add(new Circle(new Point(0, -100+0.7, 0), new Vector(0, 1, 0), 68)
+		constGeometries.add(new Circle(new Point(0, -100+0.07, 0), axisY, 68)
 									.setEmission(new Color(58, 58, 58))
 									.setMaterial(new Material().setkD(0.5).setkS(0.5).setShininess(300)));
 
-		constGeometries.add(new Circle(new Point(0, -100+0.8, 0), new Vector(0, 1, 0), 65)
+		constGeometries.add(new Circle(new Point(0, -100+0.08, 0), axisY, 65)
 									.setEmission(new Color(93, 93, 93))
 									.setMaterial(new Material().setkD(0.5).setkS(0.5).setShininess(300)));
 		
-		// scene.lights.add(new DirectionalLight(new Color(400, 255, 0), new Vector(1, -5, 1)));
-		// scene.lights.add(new DirectionalLight(new Color(400, 0, 255), new Vector(0, -5, 3)));
-		// scene.lights.add(new DirectionalLight(new Color(0, 0, 255), new Vector(-1, -5, -1)));
-		// scene.lights.add(new PointLight(new Color(500, 500, 0), new Point(40, 10 , -50))
-		// 		.setkL(0.0000003).setkQ(0.0000001));
-		// scene.lights.add(new SpotLight(new Color(0, 900, 0), new Point(-100, -70, 50), new Vector(1, -1, -2))
-		// 		.setkL(0.0000000001).setkQ(0.000000001));
+		constLights.add(new DirectionalLight(new Color(200, 200, 200), new Vector(-1, -5, -7)));
+		constLights.add(new PointLight(new Color(100, 100, 100), new Point(0, 110, 50))
+									.setkL(0.0000003).setkQ(0.0000001));
+
+		Vector lightsRadius = new Vector(60, 0, 0);
+		double lightsSize = 3;
+
+		for (int i = 0; i < 5; i++) {
+			redLightsSurfaces.add(new Circle(new Point(0, -100+0.2,0).add(lightsRadius.Roatate(72*i, axisY)),
+										axisY, lightsSize)
+											.setMaterial(new Material().setkS(0.0002).setkT(0.99)));
+		
+			redLights.add(new SpotLight(new Color(900, 400, 400),
+										new Point(0, -100+lightsSize/2,0).add(lightsRadius.Roatate(72*i, axisY)),
+										new Vector(0, -1, 0)) //
+										.setNarrowBeam(0.6).setkL(4E-5).setkQ(2E-7));
+		}
+		for (int i = 0; i < 5; i++) {
+			greenLightsSurfaces.add(new Circle(new Point(0, -100+0.2,0).add(lightsRadius.Roatate(24+72*i, axisY)),
+										axisY, lightsSize)
+											.setMaterial(new Material().setkS(0.0002).setkT(0.99)));
+		
+			greenLights.add(new SpotLight(new Color(400, 900, 400),
+										new Point(0, -100+lightsSize/2,0).add(lightsRadius.Roatate(24+72*i, axisY)),
+										new Vector(0, -1, 0)) //
+										.setNarrowBeam(0.6).setkL(4E-5).setkQ(2E-7));
+		}
+		for (int i = 0; i < 5; i++) {
+			blueLightsSurfaces.add(new Circle(new Point(0, -100+0.2,0).add(lightsRadius.Roatate(48+72*i, axisY)),
+										axisY, lightsSize)
+											.setMaterial(new Material().setkS(0.0002).setkT(0.99)));
+		
+			blueLights.add(new SpotLight(new Color(400, 400, 900),
+										new Point(0, -100+lightsSize/2,0).add(lightsRadius.Roatate(48+72*i, axisY)),
+										new Vector(0, -1, 0)) //
+										.setNarrowBeam(0.6).setkL(4E-5).setkQ(2E-7));
+		}
 
 		String imageName = new String("cool image/cool image %d");
 		ImageWriter imageWriter = new ImageWriter("", 1000, 1000);
@@ -327,15 +370,27 @@ public class RenderTests {
 			.spinRightLeft(0.0001)
 			.setImageWriter(imageWriter);	
 
-		for (int i = 0; i < 90; i += 15) {
+		for (int i = 0; i < 180; i += 15) {
 			Scene scene = new Scene("Test scene");
 			scene.geometries.add(constGeometries);
 			scene.geometries.add(choper.rotatHellicopter(i));
-			
 
-			scene.lights.add(new DirectionalLight(new Color(255, 255, 255), new Vector(-1, -5, -7)));
-			scene.lights.add(new PointLight(new Color(100, 100, 100), new Point(0, 110, 50))
-					.setkL(0.0000003).setkQ(0.0000001));
+			scene.lights.addAll(constLights);
+			
+			switch ((i/45)%3) {
+				case 0:	
+					scene.lights.addAll(redLights);
+					scene.geometries.add(redLightsSurfaces);
+					break;
+				case 1:
+					scene.lights.addAll(greenLights);
+					scene.geometries.add(greenLightsSurfaces);
+					break;
+				case 2:
+					scene.lights.addAll(blueLights);
+					scene.geometries.add(blueLightsSurfaces);
+					break;
+			}
 
 			// camera.spinRightLeft(0.001);
 
