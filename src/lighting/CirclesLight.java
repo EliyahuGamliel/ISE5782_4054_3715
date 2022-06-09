d2e022a2fd59aab7fa25a83cab9b3a33d403e057
asimon <avrahams655@gmail.com>
2022-06-09 04:17:28 +0300

    a better scatter for CircleLight

--- src/lighting/CirclesLight.java
+++ src/lighting/CirclesLight.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import primitives.*;
 
+import static primitives.Util.*;
 /**
  * a class that represents a lightsource with radius
  */
@@ -12,18 +13,20 @@ public class CirclesLight extends PointLight {
     private final Vector direction;
     private final double radius;
     private int numOfShadowRays = 72;
-
+    private double n;
     
     public CirclesLight(Color intensity, Point position, Vector direction, double radius) {
         super(intensity, position);
         this.direction = direction;
         this.radius = radius;
+        n = Math.sqrt(numOfShadowRays);
     }
 
     public CirclesLight(Color intensity, Point position,Vector direction, double radius, double kC, double kL, double kQ) {
         super(intensity, position, kC, kL, kQ);
         this.direction = direction;
         this.radius = radius;
+        n = Math.sqrt(numOfShadowRays);
     }
 
     @Override
@@ -31,15 +34,21 @@ public class CirclesLight extends PointLight {
         List<VectorDistance> res = new ArrayList<>(numOfShadowRays);
         Vector l = getL(p);
         Vector vRight = l.crossProduct(direction).normalize();
-        for (double i = 0; i < 360; i+=(360/numOfShadowRays)) {
-            Point pl = position.add(vRight.Roatate(i, direction).scale(radius));
-            res.add(new VectorDistance(p.subtract(pl).normalize(), pl.distance(p)));
+        Vector vUp = vRight.crossProduct(direction);
+        for (double j = -radius / 2; isZero(j - (radius/2)) || j <= radius / 2; j += radius / (n - 1)) {
+            for (double i = -radius / 2; isZero(i - (radius/2)) || i <= radius / 2; i += radius / (n - 1)) {
+                Point ijP = p;
+                if (!isZero(j)) ijP = ijP.add(vRight.scale(j));
+                if (!isZero(i)) ijP = ijP.add(vUp.scale(i));
+                res.add(new VectorDistance(ijP.subtract(position).normalize(), ijP.distance(position)));
+            }
         }
         return res;
     }
 
     public CirclesLight setNumOfShadowRays(int numOfShadowRays) {
         this.numOfShadowRays = numOfShadowRays;
+        n = Math.sqrt(numOfShadowRays);
         return this;
     }
 }
